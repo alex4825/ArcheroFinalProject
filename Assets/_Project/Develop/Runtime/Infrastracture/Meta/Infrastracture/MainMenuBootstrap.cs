0d9efdfe2650f3edf21685f1403d17f7517cae4e
Alexey Jukov <nitroalexus@gmail.com>
2025-07-13 16:22:45 +0600

    Reactive variable
    Subscribing
    Unsubscribing
    *Subscribing lambda expressions*

--- Assets/_Project/Develop/Runtime/Infrastracture/Meta/Infrastracture/MainMenuBootstrap.cs
+++ Assets/_Project/Develop/Runtime/Infrastracture/Meta/Infrastracture/MainMenuBootstrap.cs
@@ -1,9 +1,11 @@
 ﻿using Assets._Project.Develop.Runtime.Infrastracture.DI;
 using Assets._Project.Develop.Runtime.Infrastracture.Gameplay.Infrastracture;
 using Assets._Project.Develop.Runtime.Utilities.CoroutinesManagement;
+using Assets._Project.Develop.Runtime.Utilities.Reactive;
 using Assets._Project.Develop.Runtime.Utilities.SceneManagement;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets._Project.Develop.Runtime.Infrastracture.Meta.Infrastracture
@@ -11,6 +13,10 @@ namespace Assets._Project.Develop.Runtime.Infrastracture.Meta.Infrastracture
     public class MainMenuBootstrap : SceneBootsprap
     {
         private DIContainer _container;
+
+        private ReactiveVariable<int> _field;
+        private ReactiveVariable<int> _field2;
+        private List<IDisposable> _disposables = new List<IDisposable>();
 
         public override void ProcessRegistrations(DIContainer container, IInputSceneArgs sceneArgs = null)
         {
@@ -28,16 +34,39 @@ namespace Assets._Project.Develop.Runtime.Infrastracture.Meta.Infrastracture
         public override void Run()
         {
             Debug.Log("Старт сцены главного меню.");
+
+            _field = new ReactiveVariable<int>(5);
+            _field2 = new ReactiveVariable<int>(10);
+            _disposables.Add(_field.Subscribe(OnFieldChanged));
+            _disposables.Add(_field2.Subscribe((arg1, arg2) => Debug.Log($"Поле изменилось. Старое значение - {arg1}, новое - {arg2}")));
+        }
+
+        private void OnFieldChanged(int arg1, int arg2)
+        {
+            Debug.Log($"Поле изменилось. Старое значение - {arg1}, новое - {arg2}");
         }
 
         private void Update()
         {
-            if(Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 SceneSwitcherService sceneSwitcherService = _container.Resolve<SceneSwitcherService>();
                 ICoroutinesPerformer coroutinesPerformer = _container.Resolve<ICoroutinesPerformer>();
 
                 coroutinesPerformer.StartPerform(sceneSwitcherService.ProcesSwitchTo(Scenes.Gameplay, new GameplayInputArgs(2)));
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _field.Value++;
+                _field2.Value++;
+
+                foreach (var disposable in _disposables)
+                {
+                    disposable.Dispose();
+                }
+
+                _disposables.Clear();
             }
         }
     }
---
+++ Assets/_Project/Develop/Runtime/Utilities/Reactive/ReactiveVariable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets._Project.Develop.Runtime.Utilities.Reactive
+{
+    public class ReactiveVariable<T> : IReadonlyVariable<T> where T : IEquatable<T>
+    {
+        private List<Subscriber<T, T>> _subscribers = new();
+
+        private T _value;
+
+        public ReactiveVariable(T value) => Value = value;
+
+        public ReactiveVariable() => Value = default(T);
+
+        public T Value
+        {
+            get => _value;
+
+            set
+            {
+                T oldValue = _value;
+
+                _value = value;
+
+                if (_value.Equals(oldValue) == false)
+                    foreach (var subscriber in _subscribers)
+                        subscriber?.Invoke(oldValue, _value);
+            }
+        }
+
+        public IDisposable Subscribe(Action<T, T> action)
+        {
+            Subscriber<T, T> subscriber = new(action, Remove);
+            _subscribers.Add(subscriber);
+            return subscriber;
+        }
+
+        private void Remove(Subscriber<T, T> subscriber) => _subscribers.Remove(subscriber);
+    }
+}
