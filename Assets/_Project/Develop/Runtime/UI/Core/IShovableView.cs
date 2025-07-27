da5a557ce1b50376834290c09ed0ed03b6d9f601
Alexey Jukov <nitroalexus@gmail.com>
2025-07-27 23:36:24 +0600

    Popup view base

---
+++ Assets/_Project/Develop/Runtime/UI/Core/IShovableView.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets._Project.Develop.Runtime.UI.Core
+{
+    public interface IShovableView : IView
+    {
+        void Show();
+
+        void Hide();
+    }
+}
---
+++ Assets/_Project/Develop/Runtime/UI/Core/PopupViewBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.UI.Core
+{
+    public abstract class PopupViewBase : MonoBehaviour, IShovableView
+    {
+        public event Action CloseRequest;
+
+        [SerializeField] private CanvasGroup _mainGroup;
+
+        private void Awake()
+        {
+            _mainGroup.alpha = 0;
+        }
+
+        public void OnCloseButtonClicked() => CloseRequest?.Invoke();
+
+        public void Show()
+        {
+            OnPreShow();
+
+            //Анимация появления
+            _mainGroup.alpha = 1;
+
+            OnPostShow();
+        }
+
+        public void Hide()
+        {
+            OnPreHide();
+
+            //Анимация исчезновения
+            _mainGroup.alpha = 0;
+
+            OnPostHide();
+        }
+
+        protected virtual void OnPreShow() { }
+
+        protected virtual void OnPostShow() { }
+
+        protected virtual void OnPreHide() { }
+
+        protected virtual void OnPostHide() { }
+
+    }
+}
