690a0979f35a15a9381c1d859ba647ff310fe2cf
Alexey Jukov <nitroalexus@gmail.com>
2025-08-20 21:22:56 +0600

    Ghost's through-motion

--- Assets/_Project/Develop/Runtime/Gameplay/TestGameplay.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/TestGameplay.cs
@@ -23,6 +23,7 @@ namespace Assets._Project.Develop.Runtime.Gameplay
         public void Run()
         {
             _entity = _entitiesFactory.CreateGhost(Vector3.zero);
+            _entitiesFactory.CreateGhost(Vector3.zero + Vector3.forward * 5); 
 
             _isRunning = true;
         }
