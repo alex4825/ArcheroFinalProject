fa9cad5a7394dd571c7adcc3f4fbbaaeac041e43
Alexey Jukov <nitroalexus@gmail.com>
2025-08-30 19:45:49 +0600

    Comparer in reactive variable

--- Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Entity.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/EntitiesCore/Entity.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
 {
-    public partial class Entity : IDisposable, IEquatable<Entity>
+    public partial class Entity : IDisposable
     {
         private readonly Dictionary<Type, IEntityComponent> _components = new();
 
@@ -95,7 +95,5 @@ namespace Assets._Project.Develop.Runtime.Gameplay.EntitiesCore
 
             return this;
         }
-
-        public bool Equals(Entity other) => other == this;
     }
 }
