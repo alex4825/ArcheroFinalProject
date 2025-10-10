73f6fbdf7371011be88043cdc7ec524925bbaef4
Alexey Jukov <nitroalexus@gmail.com>
2025-10-11 00:58:47 +0600

    ReactiveEvent expanded

--- Assets/_Project/Develop/Runtime/Utilities/Reactive/IReadonlyEvent.cs
+++ Assets/_Project/Develop/Runtime/Utilities/Reactive/IReadonlyEvent.cs
@@ -11,4 +11,9 @@ namespace Assets._Project.Develop.Runtime.Utilities.Reactive
     {
         IDisposable Subscribe(Action<T> action);
     }
+
+    public interface IReadonlyEvent<T1, T2>
+    {
+        IDisposable Subscribe(Action<T1, T2> action);
+    }
 }
--- Assets/_Project/Develop/Runtime/Utilities/Reactive/ReactiveEvent.cs
+++ Assets/_Project/Develop/Runtime/Utilities/Reactive/ReactiveEvent.cs
@@ -40,22 +40,22 @@ namespace Assets._Project.Develop.Runtime.Utilities.Reactive
     }
 
 
-    public class ReactiveEvent<T> : IReadonlyEvent<T>
+    public class ReactiveEvent<T1> : IReadonlyEvent<T1>
     {
-        private List<Subscriber<T>> _subscribers = new();
-        private List<Subscriber<T>> _toAdd = new();
-        private List<Subscriber<T>> _toRemove = new();
+        private List<Subscriber<T1>> _subscribers = new();
+        private List<Subscriber<T1>> _toAdd = new();
+        private List<Subscriber<T1>> _toRemove = new();
 
-        public IDisposable Subscribe(Action<T> action)
+        public IDisposable Subscribe(Action<T1> action)
         {
-            Subscriber<T> subscriber = new(action, Remove);
+            Subscriber<T1> subscriber = new(action, Remove);
             _toAdd.Add(subscriber);
             return subscriber;
         }
 
-        private void Remove(Subscriber<T> subscriber) => _toRemove.Add(subscriber);
+        private void Remove(Subscriber<T1> subscriber) => _toRemove.Add(subscriber);
 
-        public void Invoke(T arg)
+        public void Invoke(T1 arg)
         {
             if (_toAdd.Count > 0)
             {
@@ -73,6 +73,43 @@ namespace Assets._Project.Develop.Runtime.Utilities.Reactive
 
             foreach (var subscriber in _subscribers)
                 subscriber.Invoke(arg);
+        }
+    }
+
+
+    public class ReactiveEvent<T1, T2> : IReadonlyEvent<T1, T2>
+    {
+        private List<Subscriber<T1, T2>> _subscribers = new();
+        private List<Subscriber<T1, T2>> _toAdd = new();
+        private List<Subscriber<T1, T2>> _toRemove = new();
+
+        public IDisposable Subscribe(Action<T1, T2> action)
+        {
+            Subscriber<T1, T2> subscriber = new(action, Remove);
+            _toAdd.Add(subscriber);
+            return subscriber;
+        }
+
+        private void Remove(Subscriber<T1, T2> subscriber) => _toRemove.Add(subscriber);
+
+        public void Invoke(T1 arg1, T2 arg2)
+        {
+            if (_toAdd.Count > 0)
+            {
+                _subscribers.AddRange(_toAdd);
+                _toAdd.Clear();
+            }
+
+            if (_toRemove.Count > 0)
+            {
+                foreach (var subscriber in _toRemove)
+                    _subscribers.Remove(subscriber);
+
+                _toRemove.Clear();
+            }
+
+            foreach (var subscriber in _subscribers)
+                subscriber.Invoke(arg1, arg2);
         }
     }
 }
